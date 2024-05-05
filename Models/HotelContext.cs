a2dc621a48a9a9aaa14835ccfab2365b4ff24b0a
Salma <Salma@DESKTOP-EJ1BH82>
2024-05-05 21:57:38 +0300

    Add repositary

--- Models/HotelContext.cs
+++ Models/HotelContext.cs
@@ -3,14 +3,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HotelManagement_MVC.Models
 {
+
     public class HotelContext:IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Hotel> HotelSet { get; set; }
+        public HotelContext() : base()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-EJ1BH82\\SQLEXPRESS;Initial Catalog=HotelBookingDataBase;Integrated Security=True;Encrypt=False");
+        }
         //public DbSet<HotelRoom> Room { get; set; }
         public HotelContext(DbContextOptions<HotelContext> options)
           : base(options)  { }//inject ==>IOC resolve
 
         //fill some data in table flut api
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        { }
+        {
+            //Id(PK) ,HotelName, Location, Gallery, PhoneNum, Description.
+            modelBuilder.Entity<Hotel>().HasData(new Hotel() 
+            { Id=1,Images="1.png",Description="Very Good", Location="Cairo", Name="ITI", Phone="12338587"});
+
+            base.OnModelCreating(modelBuilder);
+
         }
+
+
+    }
 }
