f444fcd2fde0305e47011b5ac8867f78bf135806
Salma <Salma@DESKTOP-EJ1BH82>
2024-05-06 18:39:20 +0300

    FrontEnd LayOut & IHotelRoomTypeRepo

---
+++ Controllers/HotelRoomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement_MVC.Controllers
+{
+    public class HotelRoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- IRepository/IHotelRoomTypeRepo.cs
+++ IRepository/IHotelRoomTypeRepo.cs
@@ -1,6 +1,18 @@
-﻿namespace HotelManagement_MVC.IRepository
+﻿using HotelManagement_MVC.Models;
+
+namespace HotelManagement_MVC.IRepository
 {
     public interface IHotelRoomTypeRepo
     {
+        List<HotelRoomType> GetAll();
+        HotelRoomType GetBuId(int Id);
+        void Update(HotelRoomType obj);
+        void Delete(int Id);
+        void Insert(HotelRoomType obj);
+        void Save();
+        List<HotelRoom> GetByRoomTypeId(int roomTypeId);
+        //public List<HotelRoomType> GetByGuest();
+        public List<HotelRoomType> Search(string search);
+            
     }
 }
