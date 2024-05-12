b24b12c92cbb89f1107bc14fe40811cf7b184a29
Salma <Salma@DESKTOP-EJ1BH82>
2024-05-13 00:43:30 +0300

    RoomType Views

--- Models/Offer.cs
+++ Models/Offer.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public string OfferName { get; set; }
         public string OfferDescription { get; set; }
-        public string OfferImage { get; set; }
-        public string OfferPrice { get; set; }
+        public string? OfferImage { get; set; }
+        public int OfferPrice { get; set; }
 
     }
 }
