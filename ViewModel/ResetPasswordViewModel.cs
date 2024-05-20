4262d84a78ef71a8fd23c5116d198521385f9c9d
Salma <Salma@DESKTOP-EJ1BH82>
2024-05-20 21:23:41 +0300

    Forget password

---
+++ ViewModel/ResetPasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelManagement_MVC.ViewModel
+{
+	public class ResetPasswordViewModel
+	{
+		[Required(ErrorMessage = "Password is requires")]
+		[DataType(DataType.Password)]
+		public string newPassword { get; set; }
+		[Required(ErrorMessage = "Conform password is requires")]
+		[Compare("newPassword", ErrorMessage = "Password does not match")]
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm Password")]
+		public string ConfirmPassword { get; set; }
+
+
+		//public string Email { get; set; }
+		//public string Token { get; set; }
+	}
+}
